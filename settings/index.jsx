58ed14bba1e478b0bb679fa45a26f565cd3a68de
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-21 23:39:25 +0200

    Add files via upload

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,6 +1,13 @@
 function mySettings(props) {
   return (
     <Page>
+        {<Text bold align="center">Secondi del refresh: </Text>}
+        <TextInput
+          label="Numero di secondi: "
+          title="seconds"
+          settingsKey="seconds"
+          type="text" 
+        />
     </Page>
   );
 }
