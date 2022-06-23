cf78f2ae051526baca3f2683883d6df5599a8721
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-23 07:36:55 +0200

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -13,6 +13,8 @@ function mySettings(props) {
       {<Text bold align="center">Creato da Giulio Sorrentino</Text>}
       {<Text bold align="center">Debbugato dal team di fitbit</Text>}
       {<Text bold align="center">Dedicato a Francesca Milano</Text>}
+      {<Text bold align="center">Questo programma è sotto licenza GPL v3 o, secondo la tua opinione, qualsiasi versione successiva</text>}
+      {<Text bold align="center">https://www.github.com/numerunix/FitFortune</Text>}
     </Page>
   );
 }
