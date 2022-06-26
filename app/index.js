762ea622ca144d36e92dce39997a03a0f8b9a1b6
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-26 18:32:01 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import document from "document";
 import  * as messaging from "messaging";
+import {me} from "companion";
 
 let cookie = document.getElementById("cookie");
 let secondi;
@@ -7,23 +8,20 @@ let secondi;
 // Message is received
 messaging.peerSocket.onmessage = evt => {
   console.log(`App received: ${JSON.stringify(evt)}`);
-    if (evt.data.key === "seconds" && evt.data.newValue) {
-      secondi=evt.data.newValue*1000;
   }
-  cookie.text = JSON.stringify(evt);
- }
+}
 
 // Message socket opens
 messaging.peerSocket.onopen = () => {
-  console.log("App Socket Open");;
+  console.log("Copyright \u00A92022 Giulio Sorrentino<gsorre84@gmail.com>");
+  console.log("This progam is dedicated to Francesca Milano");
+  console.log("This program is based on MicroDateFrom");
+  console.log("This program is distribuited under GPL. No Warranty is provided.");
+  console.log("App Socket Open");
+  setTimeout(me.getNewCookie, me.secondi);
 };
                        
 // Message socket closes
 messaging.peerSocket.onclose = () => {
   console.log("App Socket Closed");
 };
-
-console.log("Copyright \u00A92022 Giulio Sorrentino<gsorre84@gmail.com>");
-console.log("This progam is dedicated to Francesca Milano");
-console.log("This program is based on MicroDateFrom");
-console.log("This program is distribuited under GPL. No Warranty is provided.");
