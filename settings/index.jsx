afb52ef8abb8f95cac544a203b62250c148c14f6
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-07-21 14:41:13 +0200

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -18,7 +18,7 @@ function mySettings(props) {
       <Button list label="Elimina impostazioni" onClick={() => props.settingsStorage.clear()} />
       {<Text bold align="center">Creato da Giulio Sorrentino e sviluppato con l'aiuto del team americano di fitbit</Text>}
       {<Text bold align="center">Debbugato dal team americano di fitbit e dalla cominità americana di fitbit</Text>}
-      {<Text bold align="center">Dedicato alla barista del Max Bar di Rivisondoli con cui c'era alchimia</Text>}
+      {<Text bold align="center">Dedicato a Francesca Milano</Text>}
       {<Text bold align="center">Questo programma è sotto licenza GPL v3 o, secondo il tuo parere, qualsiasi versione successiva</Text>}
       {<Text bold align="center">https://www.github.com/numerunix/FitFortune</Text>}
         </Section>
