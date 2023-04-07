f2b78761bcfa96f729f6db0ca68596c1c3ba308c
Giulio Sorrentino <gsorre84@gmail.com>
2023-04-07 15:54:02 +0200

    now the program is the correct one

--- fitfortune/settings/index.jsx
+++ fitfortune/settings/index.jsx
@@ -1,57 +1,15 @@
 function mySettings(props) {
   return (
     <Page>
-      <Section
-        title={<Text bold align="center">Persona amata</Text>}>
-        <TextInput
-          label="Nome: "
-          title="Nome0"
-          settingsKey="nome"
-          type="text" 
-        />
-        <TextInput
-          label="Giorno di conoscenza (aaa-mm-gg): "
-          title="Giorno0"
-          settingsKey="day"
-          type="text" 
-        />
-      </Section>
-      <Section
-        title={<Text bold align="center">Seconda Persona</Text>}>
-        <TextInput
-          label="Nome: "
-          title="Nome1"
-          settingsKey="nome1"
-          type="text"
-        />
-        <TextInput
-          label="Giorno di conoscenza (aaa-mm-gg): "
-          title="Giorno1"
-          settingsKey="day1"
-          type="text" 
-        />
-      </Section>
-      <Section
-        title={<Text bold align="center">Terza persona</Text>}>
-        <TextInput
-          label="Nome: "
-          title="Nome2"
-          settingsKey="nome2"
-          type="text" 
-        />
-        <TextInput
-          label="Giorno di conoscenza (aaa-mm-gg): "
-          title="Giorno2"
-          settingsKey="day2"
-          type="text" 
-        />
-       <Button list label="Elimina impostazioni" onClick={() => props.settingsStorage.clear()} />
-      {<Text bold align="center">Creato da Giulio Sorrentino</Text>}
+      <Section>
+      {<Text bold align="center">Creato da Giulio Sorrentino e sviluppato con l'aiuto del team americano di fitbit</Text>}
+      {<Text bold align="center">Debbugato dal team americano di fitbit e dalla cominità americana di fitbit</Text>}
       {<Text bold align="center">Dedicato a Francesca Milano</Text>}
       {<Text bold align="center">Questo programma è sotto licenza GPL v3 o, secondo il tuo parere, qualsiasi versione successiva</Text>}
-      {<Text bold align="center">https://www.github.com/numerunix/DateFrom</Text>}
-      </Section>
-    </Page>
+      {<Text bold align="center">https://www.github.com/numerunix/FitFortune</Text>}
+        </Section>
+      </Page>
+    
   );
 }
 
